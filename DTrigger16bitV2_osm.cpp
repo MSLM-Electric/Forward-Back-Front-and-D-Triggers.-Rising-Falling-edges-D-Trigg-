13fd80c29bf0bd7ad752c9a3a068279c453da500
MSLM Electric <59217275+MSLM-Electric@users.noreply.github.com>
2021-10-07 17:57:45 +0500

    Add files via upload

---
+++ DTrigger16bitV2_osm.cpp
@@ -0,0 +1,146 @@
+#include "DTrigger16bitV2_osm.h"
+
+/*
+  Copyright (c) 2019 Abdulhamidov Osim
+  All rights reserved.
+
+THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
+AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
+ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDER OR CONTRIBUTORS BE
+LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR
+CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF
+SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS
+INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN
+CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE)
+ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
+POSSIBILITY OF SUCH DAMAGE.
+*/
+
+
+/*		_________________
+	___|					a (uint) - 16 x IN signals or commandVars 		
+		__
+	___|  |______________	FwdFrntT.Qff  16 x Outs - Calculating process is logical parallel
+*/
+unsigned int ForwardFrontTrigger16bit(unsigned int a, unsigned int tBITx, ForwardFrontsInternalRegs16bit_t *FFRegs)
+{
+	/*if (a == 1)
+	{
+		a = 0xFFFF;
+	}*/
+	a = a & tBITx;
+	(*FFRegs).inputSignal = ((*FFRegs).inputSignal & ~tBITx) | a;
+	(*FFRegs).Qff = (*FFRegs).inputSignal & (~(*FFRegs).FfrontRegister);
+		(*FFRegs).FfrontRegister = (*FFRegs).inputSignal;
+	return (*FFRegs).Qff;        //FFRegs->Qff;
+}
+
+//EXAMPLE of using
+/*
+#include <xc.h>
+#include "DTrigger_osm.h"
+
+#define ON 1
+#define OFF 0
+#define DI_RUNMotor TRISBbits.7
+#define DI_RUNPump TRISBbits.5
+#define DI_STOPMotor TRISBbits.6
+#define DQ_RUNMotor TRISAbits.0
+#define DQ_RUNPummp TRISAbits.1
+
+ForwardFrontsInternalRegs_t ForwardFTriggerReg;
+
+int main(void)
+{
+    INIT();	
+	while (true)
+	{
+		if (ForwardFrontTriggerbyte(DI_RUNMotor, tBIT0, &ForwardFTriggerReg) == tBIT0)
+		{
+			DQ_RUNMotor = ON;
+		}
+		if (ForwardFrontTriggerbyte(DI_STOPMotor, tBIT1, &ForwardFTriggerReg) == tBIT1)
+		{
+		    DQ_RUNMotor = OFF;
+		}
+	}
+}//*/
+
+//Пример параллельного считывания и определения присутствия переднего фронта 3 случайных входных сигналов:
+//DI_RUNMotor - tBIT0
+//DI_RUNPummp - tBIT5
+//ReadTSensorOnce - tBIT4
+//DI_STOPMotor - tBIT1
+//Зарисовать один раз Меню настроек в LCD - tBIT15
+/*
+ForwardFrontsInternalRegs16bit_t ForwardFTriggerReg;
+int main(void)
+{
+	INIT();
+	while (true)
+	{
+		ForwardFrontTrigger16bit((DI_RUNMotor * tBIT0) | (DI_RUNPump * tBIT5) | (ReadTSensorOnce * tBIT4) | (1 * tBIT15), tBIT0 | tBIT5 | tBIT4 | tBIT15, &ForwardFTriggerReg);
+		if((ForwardFTriggerReg.Qff & tBIT0) == tBIT0)
+		{
+			DQ_RUNMotor = ON;
+		}
+		if((ForwardFTriggerReg.Qff & tBIT5) == tBIT5)
+		{
+			DI_RUNPump = ON;
+		}
+		if((ForwardFTriggerReg.Qff & tBIT4) == tBIT4)
+		{
+			ds18b20ReadTemp(ds18b20_1wireUnit, Tsensor2Address, data);
+		}
+		if((ForwardFTriggerReg.Qff & tBIT15) == tBIT15)
+		{
+			DrawSettinsMenuByLCD();
+		}
+	}
+}
+*/
+
+/*		__		  _____			  _			 ______
+	___|  |______|	   |_________| |________|	   |___		a (uint) - 16 x IN signals or commandVars
+		_________				  __________
+	___|		 |_______________|			|__________		DTrig.Qff  16 x Outs - Calculating process is logical parallel
+*/
+unsigned int D_Trigger16bit(unsigned int a, unsigned int tBITx, D_TriggersInternalRegs16bit_t *DTrigRegs)
+{
+	a = a & tBITx;
+	(*DTrigRegs).inputSignal = ((*DTrigRegs).inputSignal & (~tBITx)) | a;
+	(*DTrigRegs).Q_Dtrig = ((*DTrigRegs).inputSignal | (*DTrigRegs).Q_Dtrig) & (~(*DTrigRegs).DtrigRegK2);
+	(*DTrigRegs).DtrigRegK1 = (*DTrigRegs).Q_Dtrig & (~(*DTrigRegs).inputSignal | (*DTrigRegs).DtrigRegK1);
+	(*DTrigRegs).DtrigRegK2 = ((*DTrigRegs).DtrigRegK1 | (*DTrigRegs).DtrigRegK2) & (*DTrigRegs).inputSignal;
+	return (*DTrigRegs).Q_Dtrig;
+}
+
+/*	_______
+		   |_____________	a (uint) - 16 x IN signals or commandVars
+		    __
+	_______|  |__________	BackFrntT.Qbf
+
+	   --->|--|<--- Tbf = Tff - 1 cycle fully executed program
+*/
+unsigned int BackFrontTrigger16bit(unsigned int a, unsigned int tBITx, BackFrontsInternalRegs16bit_t *BFRegs)
+{
+	a = a & tBITx;
+	(*BFRegs).inputSignal = ((*BFRegs).inputSignal & (~tBITx)) | a;
+	(*BFRegs).BfrontRegister = ((*BFRegs).inputSignal | (*BFRegs).BfrontRegister) & (~(*BFRegs).Qbf);
+	(*BFRegs).Qbf = ~(*BFRegs).inputSignal & (*BFRegs).BfrontRegister;
+	return (*BFRegs).Qbf;
+}
+
+
+unsigned char delayedPress16bit(unsigned int *countReg, unsigned char inputBit, unsigned char k)  //int have not sense
+{
+	if (inputBit != 0) { *countReg = *countReg + 1; }
+	else { *countReg = 0; }
+	if (*countReg == k)
+	{
+		*countReg = 0;
+		return 1;
+	}
+	return 0;
+}
---
+++ DTrigger16bitV2_osm.h
@@ -0,0 +1,81 @@
+//#pragma once
+#ifndef DTRIGGER16BITV2_OSM_h
+#define DTRIGGER16BITV2_OSM_h
+
+#if DTRIGGERV2_OSM_h
+#include "DTriggerV2_osm.h"  //8bit implementation
+#endif
+
+
+/******************defines********************/
+#define	tBIT15  0x8000
+#define	tBIT14  0x4000
+#define	tBIT13  0x2000
+#define	tBIT12  0x1000
+#define	tBIT11  0x0800
+#define	tBIT10  0x0400
+#define	tBIT9  0x0200
+#define	tBIT8  0x0100
+
+#ifndef DTRIGGERV2_OSM_h
+#define	tBIT7  0x0080
+#define	tBIT6  0x0040
+#define	tBIT5  0x0020
+#define	tBIT4  0x0010
+#define	tBIT3  0x0008
+#define	tBIT2  0x0004
+#define	tBIT1  0x0002
+#define	tBIT0  0x0001
+#endif
+/**********************************************/
+
+//typedef enum
+//{
+//	tBIT15 = 0x8000,
+//	tBIT14 = 0x4000,
+//	tBIT13 = 0x2000,
+//	tBIT12 = 0x1000,
+//	tBIT11 = 0x0800,
+//	tBIT10= 0x0400,
+//	tBIT9 = 0x0200,
+//	tBIT8 = 0x0100,
+//	tBIT7 = 0x0080,
+//	tBIT6 = 0x0040,
+//	tBIT5 = 0x0020,
+//	tBIT4 = 0x0010,
+//	tBIT3 = 0x0008,
+//	tBIT2 = 0x0004,
+//	tBIT1 = 0x0002,
+//	tBIT0 = 0x0001
+//}BITxmask_type;
+
+struct ForwardFrontsInternalRegs16bit_t
+{
+	unsigned int inputSignal;
+	unsigned int FfrontRegister;
+	unsigned int Qff;
+};
+
+struct BackFrontsInternalRegs16bit_t  //Эта структура похожа на верхнюю, так что можно обобщать-объединять (т.е это удалять и верхнее универсализовав применять)
+{
+	unsigned int inputSignal;
+	unsigned int BfrontRegister;
+	unsigned int Qbf;
+};
+
+struct D_TriggersInternalRegs16bit_t
+{
+	unsigned int DtrigRegK1;
+	unsigned int DtrigRegK2;
+	unsigned int inputSignal;
+	unsigned int Q_Dtrig;
+};
+
+unsigned int ForwardFrontTrigger16bit(unsigned int a, unsigned int tBITx, ForwardFrontsInternalRegs16bit_t *FFRegs);
+unsigned int BackFrontTrigger16bit(unsigned int a, unsigned int tBITx, BackFrontsInternalRegs16bit_t *BFRegs);
+unsigned int D_Trigger16bit(unsigned int a, unsigned int tBITx, D_TriggersInternalRegs16bit_t *DTrigRegs);
+unsigned char delayedPress16bit(unsigned int *countReg, unsigned char inputBit, unsigned char k);
+
+
+#endif // !DTRIGGER16BITV2_OSM_h
+
